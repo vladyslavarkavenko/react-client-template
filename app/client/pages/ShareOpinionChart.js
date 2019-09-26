bd734535dc0f10cd2993de443aabdc3a0013a120
Vladislav Arkavenko <v.arkavenko@skywell.software>
2019-09-26 17:40:44 +0300

    Commit before merge with dev

--- app/client/pages/ShareOpinionChart.js
+++ app/client/pages/ShareOpinionChart.js
@@ -1,42 +1,82 @@
+/* eslint-disable */
 import React from 'react';
 import ReactSVG from 'react-svg';
 import { VictoryScatter, VictoryChart, VictoryVoronoiContainer, VictoryAxis } from 'victory';
 
 import '../assets/styles/pages/chart.less';
 
+const ticks = 10;
+const padding = 50;
+const height = 600;
+const width = 1500;
+
 const allPoints = [];
-for (let i = 1; i <= 10; i += 1) {
-  for (let s = 1; s <= 10; s += 1) {
-    allPoints.push({ importance: i, satisfaction: s });
+for (let i = 1; i <= ticks; i += 1) {
+  for (let s = 1; s <= ticks; s += 1) {
+    allPoints.push({
+      importance: i,
+      satisfaction: s
+    });
   }
 }
 
 const data = [
-  { importance: 1, satisfaction: 5, opinions: 1 },
-  { importance: 2, satisfaction: 7, opinions: 50 },
-  { importance: 3, satisfaction: 4, opinions: 75 },
-  { importance: 8, satisfaction: 4, opinions: 100 }
+  {
+    importance: 1,
+    satisfaction: 5,
+    opinions: 1
+  },
+  {
+    importance: 2,
+    satisfaction: 7,
+    opinions: 50
+  },
+  {
+    importance: 3,
+    satisfaction: 4,
+    opinions: 75
+  },
+  {
+    importance: 8,
+    satisfaction: 4,
+    opinions: 100
+  }
 ];
 
 const colors = {
-  lt: { r: 255, g: 0, b: 0 }, //    left-top
-  rt: { r: 255, g: 153, b: 0 }, //  right-top
-  rb: { r: 51, g: 204, b: 51 }, //  right-bottom
-  lb: { r: 51, g: 51, b: 255 } //   left-bottom
+  lt: {
+    r: 255,
+    g: 0,
+    b: 0
+  }, //    left-top
+  rt: {
+    r: 255,
+    g: 153,
+    b: 0
+  }, //  right-top
+  rb: {
+    r: 51,
+    g: 204,
+    b: 51
+  }, //  right-bottom
+  lb: {
+    r: 51,
+    g: 51,
+    b: 255
+  } //   left-bottom
 };
 
 const axisStyle = {
-  axis: { stroke: 'white', strokeWidth: 0.5 },
+  axis: {
+    stroke: 'white',
+    strokeWidth: 0.5
+  },
   axisLabel: {
     fontSize: 16,
     fill: 'white',
     fontFamily: "'Muli', sans-serif"
   }
 };
-
-const padding = 50;
-const height = 600;
-const width = 1500;
 
 const calculateColorParam = (param, x, y, width, height) => {
   const { lt, rt, rb, lb } = colors;
@@ -79,22 +119,37 @@ class ShareOpinionChart extends React.Component {
 
   onActivated(points) {
     const { satisfaction, importance } = points[0];
-    this.setState({ activePoint: { satisfaction, importance } });
+    console.log('points', points);
+    this.setState({
+      activePoint: {
+        satisfaction,
+        importance
+      }
+    });
   }
 
   onMouseMove(e) {
     const rect = e.currentTarget.getBoundingClientRect();
     const x = e.pageX - rect.left;
-    const y = height - (e.pageY - rect.top); // To reverse y-axis
+    const y = e.pageY - rect.top; // To reverse y-axis
 
-    this.setState({ x, y }, () => {
-      window.requestAnimationFrame(this.updateBG);
-    });
+    this.setState(
+      {
+        x,
+        y
+      },
+      () => {
+        window.requestAnimationFrame(this.updateBG);
+      }
+    );
   }
 
   updateBG() {
     let { x, y } = this.state;
     const chartWrapper = this.chartWrapper.current;
+
+    // Reverse y-axis
+    y = height - y;
 
     // Remove padding
     x -= padding;
@@ -123,68 +178,96 @@ class ShareOpinionChart extends React.Component {
   render() {
     const { activePoint, showOpinions } = this.state;
 
+    const Indicator = () => {
+      const { importance: i, satisfaction: s } = activePoint;
+
+      return (
+        <div
+          className="custom-indicator p-absolute"
+          style={{
+            bottom: padding,
+            left: padding,
+            top: ((ticks - i) * (height - 2 * padding)) / ticks + padding,
+            right: ((ticks - s) * (width - 2 * padding)) / ticks + padding
+          }}
+        >
+          <div className="circle lt">{i}</div>
+          <div className="circle rb">{s}</div>
+          <div className="circle rt big" />
+          <div className="circle rt medium" />
+          <div className="circle rt small" />
+        </div>
+      );
+    };
+
     return (
       <div className="chart-wrapper" ref={this.chartWrapper}>
-        {showOpinions ? (
-          <>
-            <VictoryChart
-              width={width}
-              height={height}
-              padding={padding}
-              domain={[0, 10]}
-              containerComponent={
-                <VictoryVoronoiContainer
-                  responsive={false}
-                  voronoiBlacklist={['activePoint']}
-                  labels={({ datum: { importance, satisfaction, opinions } }) =>
-                    `Importance: ${importance}\nSatisfaction: ${satisfaction}\nOpinions: ${opinions}`
-                  }
+        <div
+          className="p-relative"
+          width={width}
+          height={height}
+          onMouseMove={showOpinions ? null : this.onMouseMove}
+        >
+          <Indicator />
+          {showOpinions ? (
+            <>
+              <VictoryChart
+                width={width}
+                height={height}
+                padding={padding}
+                domain={[0, ticks]}
+                containerComponent={
+                  <VictoryVoronoiContainer
+                    responsive={false}
+                    voronoiBlacklist={['activePoint']}
+                    labels={({ datum: { importance, satisfaction, opinions } }) =>
+                      `Importance: ${importance}\nSatisfaction: ${satisfaction}\nOpinions: ${opinions}`
+                    }
+                  />
+                }
+              >
+                <VictoryAxis
+                  tickFormat={() => ''}
+                  offsetY={padding}
+                  label="Satisfaction"
+                  style={axisStyle}
                 />
-              }
-            >
-              <VictoryAxis
-                tickFormat={() => ''}
-                offsetY={padding}
-                label="Satisfaction"
-                style={axisStyle}
-              />
-              <VictoryAxis
-                style={axisStyle}
-                dependentAxis
-                tickFormat={() => ''}
-                offsetX={padding}
-                label="Importance"
-              />
-              <VictoryScatter
-                style={{ data: { fill: 'rgba(255, 255, 255, .5)' } }}
-                data={data}
-                y="importance"
-                x="satisfaction"
-                bubbleProperty="opinions"
-                maxBubbleSize={70}
-                minBubbleSize={10}
-              />
-            </VictoryChart>
+                <VictoryAxis
+                  style={axisStyle}
+                  dependentAxis
+                  tickFormat={() => ''}
+                  offsetX={padding}
+                  label="Importance"
+                />
+                <VictoryScatter
+                  style={{ data: { fill: 'rgba(255, 255, 255, .5)' } }}
+                  data={data}
+                  y="importance"
+                  x="satisfaction"
+                  bubbleProperty="opinions"
+                  maxBubbleSize={70}
+                  minBubbleSize={10}
+                />
+              </VictoryChart>
 
-            <div className="info-msg">
-              <h1 className="uppercase">Your rate</h1>
-              <ReactSVG
-                className="emoji"
-                src={`/assets/svg/emoji/${activePoint.satisfaction}_${activePoint.importance}.svg`}
-              />
-              <h3>
-                Importance {activePoint.importance} & Satisfaction {activePoint.satisfaction}
-              </h3>
-              <button> Next </button>
-            </div>
-          </>
-        ) : (
-          <div width={width} height={height} onMouseMove={this.onMouseMove}>
+              <div className="info-msg">
+                <h1 className="uppercase">Your rate</h1>
+                <ReactSVG
+                  className="emoji"
+                  src={`/assets/svg/emoji/${activePoint.satisfaction}_${activePoint.importance}.svg`}
+                />
+                <h3>
+                  Importance {activePoint.importance} & Satisfaction {activePoint.satisfaction}
+                </h3>
+                <button> Next</button>
+              </div>
+            </>
+          ) : (
             <VictoryChart
               width={width}
               height={height}
               padding={padding}
-              domain={[0, 10]}
+              domain={[0, ticks]}
               containerComponent={
                 <VictoryVoronoiContainer
                   responsive={false}
@@ -226,8 +309,8 @@ class ShareOpinionChart extends React.Component {
                 }}
               />
             </VictoryChart>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     );
   }
