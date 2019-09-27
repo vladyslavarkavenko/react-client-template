7a0149e6a06d3e8bcf6b6bfb41339d9e82010ad5
Max Melnychuk <m.melnychyk@skywell.software>
2019-09-27 14:05:16 +0300

    Share Opinion Comments layout 50%

--- app/client/pages/shareOpinion/Comments.js
+++ app/client/pages/shareOpinion/Comments.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import OpinionAboutBlock from './OpinionAboutBlock';
+import YesSvg from '../../../../public/assets/svg/check-circle.light.svg';
+import NoSvg from '../../../../public/assets/svg/times-circle.light.svg';
+import AlarmClockSvg from '../../../../public/assets/svg/alarm-clock.svg';
+import Alert from '../../components/ui-components/Alert';
 
 export default function Comments() {
   return (
-    <div className="rate-opinion content">
+    <section className="rate-opinion content">
       <OpinionAboutBlock />
-    </div>
+
+      <div className="container">
+        <div className="opinion-rec">
+          <p className="opinion-rec__title">Are you going to recommend Rene Meier to friends</p>
+          <div className="opinion-rec__list">
+            <button type="button" className="opinion-rec__btn active">
+              <YesSvg />
+              Yes
+            </button>
+
+            <button type="button" className="opinion-rec__btn">
+              <YesSvg />
+              Not sure
+            </button>
+
+            <button type="button" className="opinion-rec__btn">
+              <NoSvg />
+              No
+            </button>
+          </div>
+          <div className="opinion-rec__remind">
+            {/*<button className="ask-btn">Ask me later</button>*/}
+            {true && (
+              <Alert type={Alert.info} icon={<AlarmClockSvg />} message="We will ask you later" />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
