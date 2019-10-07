2414851b1577e68fdf6d5537b6ace1faa8497dea
Max Melnychuk <m.melnychyk@skywell.software>
2019-10-07 15:01:36 +0300

    Companies selector filter company without manager

--- app/client/modules/companies/companiesSelectors.js
+++ app/client/modules/companies/companiesSelectors.js
@@ -1,38 +1,23 @@
-import authSelectors from '../auth/authSelectors';
-
 const getCompanyData = (state) => state.companies.data;
 
 const getCompaniesList = (state) => Object.values(getCompanyData(state));
 
-const getManagersList = (state) => getCompaniesList(state).map((company) => company.manager);
+const getManagersList = (state) => {
+  const companies = getCompaniesList(state);
+  const managers = [];
 
-const getCurrentManager = (state) => {
-  const companies = getCompanyData(state);
-  const permissions = authSelectors.rolesPermissions(state);
-  const role = authSelectors.activeRole(state);
-
-  return companies[permissions[role]].manager;
-};
-
-const getManagersWithCompanies = (state) => {
-  const data = getCompanyData(state);
-
-  const companyIds = Object.keys(data);
-
-  const list = {};
-
-  companyIds.forEach((companyId) => {
-    list[companyId] = getCompanyData(state).manager;
+  companies.forEach((company) => {
+    if (company.manager) {
+      managers.push(company.manager);
+    }
   });
 
-  return list;
+  return managers;
 };
 
 export default {
   getCompaniesList,
   getManagersList,
-  getManagersWithCompanies,
-  getCurrentManager,
   data: getCompanyData,
   errors: (state) => state.companies.errors,
   activeEditCompany: (state) => state.companies.activeEditCompany,
