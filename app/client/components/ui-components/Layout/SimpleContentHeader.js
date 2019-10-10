7663cfacfe6fc74b63f18a73de44c861a362630c
Max Melnychuk <m.melnychyk@skywell.software>
2019-10-10 17:12:44 +0300

    Enhanced Content header with custom buttons

--- app/client/components/ui-components/Layout/SimpleContentHeader.js
+++ app/client/components/ui-components/Layout/SimpleContentHeader.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default function SimpleContentHeader({ title, subTitle }) {
+export default function SimpleContentHeader({ title, subTitle, customButtons }) {
   return (
     <div className="content-header">
       <div className="info simple">
-        {title && <h1>{title}</h1>}
-        {subTitle && <h2>{subTitle}</h2>}
+        <div className="text-block">
+          {title && <h1>{title}</h1>}
+          {subTitle && <h2>{subTitle}</h2>}
+        </div>
+        {customButtons && <div className="buttons-block">{customButtons}</div>}
       </div>
     </div>
   );
