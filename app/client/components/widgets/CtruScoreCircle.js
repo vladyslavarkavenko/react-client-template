cafd5049e947862b89788c174e9d0d86ea7f7858
Max Melnychuk <m.melnychyk@skywell.software>
2019-10-16 12:19:50 +0300

    CircleWithBackground component

--- app/client/components/widgets/CtruScoreCircle.js
+++ app/client/components/widgets/CtruScoreCircle.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { VictoryPie } from 'victory';
+
+import CircleWithBackground from './CircleWithBackground';
+import ArrowSvg from '../../../../public/assets/svg/sort-up.solid.svg';
 
 const config = {
   size: 300,
@@ -23,76 +25,45 @@ const circleInner = {
   color: '#00BFDF'
 };
 
-const CircleWithBackground = ({ circle, percent }) => {
-  const { size, cornerRadius, backgroundColor } = config;
-  const { innerRadius, padding, color } = circle;
-  const data = [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
-
-  return (
-    <>
-      <VictoryPie
-        standalone={false}
-        width={size}
-        height={size}
-        data={[{ x: 1, y: 100 }]}
-        innerRadius={innerRadius}
-        cornerRadius={0}
-        padding={padding}
-        labels={() => null}
-        style={{
-          data: {
-            fill: backgroundColor
-          }
-        }}
-      />
-      <VictoryPie
-        standalone={false}
-        width={size}
-        height={size}
-        data={data}
-        innerRadius={innerRadius}
-        cornerRadius={cornerRadius}
-        padding={padding}
-        labels={() => null}
-        style={{
-          data: {
-            fill: ({ datum }) => {
-              return datum.x === 1 ? color : 'transparent';
-            }
-          }
-        }}
-      />
-    </>
-  );
-};
-
-export default function CtruScoreCircle({ profileLabel, reviewsCount }) {
+export default function CtruScoreCircle({ profileLabel, reviewsCount, isDouble }) {
   const { size } = config;
 
   return (
     <div className="ctru-circle">
       <div className="ctru-circle__chart">
         <svg viewBox={`0 0 ${size} ${size}`} width="100%" height="100%">
-          <CircleWithBackground circle={circleOuter} percent={75} />
-          <CircleWithBackground circle={circleInner} percent={60} />
+          <CircleWithBackground circle={circleOuter} percent={75} config={config} />
+
+          {isDouble && <CircleWithBackground circle={circleInner} percent={60} config={config} />}
         </svg>
 
         <div className="ctru-circle__label">
           <span className="count">7.6</span>
+          <span className="diff green">
+            +12.2% <ArrowSvg />
+          </span>
+
+          {/*<span className="diff red">*/}
+          {/*  +12.2% <ArrowSvg />*/}
+          {/*</span>*/}
         </div>
       </div>
-      <ul className="ctru-circle__legend">
-        <li className="legend-item">
-          <span className="point outer" />
-          <span className="text">All clients</span>
-          <span className="score">7.9</span>
-        </li>
-        <li className="legend-item">
-          <span className="point inner" />
-          <span className="text">Clients of {profileLabel}</span>
-          <span className="score">6.9</span>
-        </li>
-      </ul>
+
+      {isDouble && (
+        <ul className="ctru-circle__legend">
+          <li className="legend-item">
+            <span className="point outer" />
+            <span className="text">All clients</span>
+            <span className="score">7.9</span>
+          </li>
+          <li className="legend-item">
+            <span className="point inner" />
+            <span className="text">Clients of {profileLabel}</span>
+            <span className="score">6.9</span>
+          </li>
+        </ul>
+      )}
+
       <span className="ctru-circle__info">Based on {reviewsCount} reviews</span>
     </div>
   );
--- app/client/components/widgets/ParticipationCircle.js
+++ app/client/components/widgets/ParticipationCircle.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { VictoryPie } from 'victory';
+
+import CircleWithBackground from './CircleWithBackground';
 
 const config = {
   size: 300,
@@ -14,48 +15,15 @@ const config = {
 export default function ParticipationCircle({ data, profileLabel = '' }) {
   const { numberUniqueCustomerOpinions: opinionsCount, numberCustomers: customerCount } = data;
 
-  const { size, innerRadius, cornerRadius, padding, color, backgroundColor } = config;
+  const { size } = config;
 
   const percent = Math.round((customerCount - opinionsCount) / customerCount) * 100;
-
-  const points = [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
 
   return (
     <div className="participation">
       <div className="participation__chart">
         <svg viewBox={`0 0 ${size} ${size}`} width="100%" height="100%">
-          <VictoryPie
-            standalone={false}
-            width={size}
-            height={size}
-            data={[{ x: 1, y: 100 }]}
-            innerRadius={innerRadius}
-            cornerRadius={0}
-            padding={padding}
-            labels={() => null}
-            style={{
-              data: {
-                fill: backgroundColor
-              }
-            }}
-          />
-          <VictoryPie
-            standalone={false}
-            width={size}
-            height={size}
-            data={points}
-            innerRadius={innerRadius}
-            cornerRadius={cornerRadius}
-            padding={padding}
-            labels={() => null}
-            style={{
-              data: {
-                fill: ({ datum }) => {
-                  return datum.x === 1 ? color : 'transparent';
-                }
-              }
-            }}
-          />
+          <CircleWithBackground percent={percent} circle={config} config={config} />
         </svg>
 
         <div className="participation__label">
