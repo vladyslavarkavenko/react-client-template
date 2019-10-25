f646898dc62ada9a4397997b02a5c7437b506e92
Max Melnychuk <m.melnychyk@skywell.software>
2019-10-25 13:23:05 +0300

    Filters layout example

--- app/client/pages/benchmarks/FilterSidebar.js
+++ app/client/pages/benchmarks/FilterSidebar.js
@@ -6,8 +6,10 @@ import { connect } from 'react-redux';
 import { clearAllBodyScrollLocks, disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 import TimesSvg from '../../../../public/assets/svg/times.svg';
+import ArrowSvg from '../../../../public/assets/svg/arrow-down.svg';
 import benchmarksSelectors from '../../modules/benchmarks/benchmarksSelectors';
 import { toggleFilterSidebar } from '../../modules/benchmarks/benchmarksActions';
+import CheckboxInput from '../../components/ui-components/Form/CheckboxInput';
 
 class FilterSidebar extends React.Component {
   constructor(props) {
@@ -23,7 +25,7 @@ class FilterSidebar extends React.Component {
     const { isOpen } = this.props;
     if (prevProps.isOpen !== isOpen) {
       if (isOpen) {
-        disableBodyScroll(this.targetElement);
+        disableBodyScroll(this.targetElement, { reserveScrollBarGap: true });
       } else {
         enableBodyScroll(this.targetElement);
       }
@@ -33,21 +35,22 @@ class FilterSidebar extends React.Component {
   componentDidMount() {
     this.targetElement = this.wrapper.current;
 
-    this.wrapper.current.addEventListener('click', this.closeModalHandler);
+    // this.wrapper.current.addEventListener('click', this.closeModalHandler);
     const { isOpen } = this.props;
 
     if (isOpen) {
-      disableBodyScroll(this.targetElement);
+      disableBodyScroll(this.targetElement, { reserveScrollBarGap: true });
     }
   }
 
   componentWillUnmount() {
     clearAllBodyScrollLocks();
-    this.wrapper.current.removeEventListener('click', this.closeModalHandler);
+    // this.wrapper.current.removeEventListener('click', this.closeModalHandler);
   }
 
-  closeModalHandler({ target }) {
-    if (!target.closest('filter-sidebar__wrapper')) {
+  closeModalHandler({ target, currentTarget }) {
+    console.log(target);
+    if (currentTarget === target) {
       const { handleModal } = this.props;
       handleModal();
     }
@@ -57,8 +60,99 @@ class FilterSidebar extends React.Component {
     const { children, title, subtitle, handleModal, isOpen } = this.props;
 
     const template = (
-      <aside className={`filter-sidebar__wrapper ${isOpen ? 'active' : ''}`} ref={this.wrapper}>
-        <div className="filter_sidebar__body">Content</div>
+      <aside
+        className={`filter-sidebar__wrapper ${isOpen ? 'active' : ''}`}
+        ref={this.wrapper}
+        onClick={this.closeModalHandler}
+      >
+        <div className="filter-sidebar__inner">
+          <div className="filter-sidebar__content">
+            <div className="filter-sidebar__head">
+              <p className="title">
+                Filters
+                <span className="close-btn" onClick={() => handleModal()}>
+                  <TimesSvg />
+                </span>
+              </p>
+
+              <ul className="selected-filters__list">
+                <li className="selected-filters__item">
+                  <span className="label">Good performance</span>
+                  <button type="button" className="delete-btn">
+                    <TimesSvg />
+                  </button>
+                </li>
+
+                <li className="selected-filters__item">
+                  <span className="label">Safe</span>
+                  <button type="button" className="delete-btn">
+                    <TimesSvg />
+                  </button>
+                </li>
+
+                <li className="selected-filters__item">
+                  <span className="label">Exclusive</span>
+                  <button type="button" className="delete-btn">
+                    <TimesSvg />
+                  </button>
+                </li>
+              </ul>
+            </div>
+
+            <div className="filter-sidebar__body">
+              <ul className="subject__list">
+                <li className="subject__item active">
+                  <button type="button" className="head">
+                    <span className="label">Mortage</span>
+
+                    <div className="controls">
+                      <span className="count">3</span>
+                      <ArrowSvg />
+                    </div>
+                  </button>
+
+                  <ul className="topic__list">
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Good performance" />
+                    </li>
+
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Low commissions" checked />
+                    </li>
+
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Low fees" />
+                    </li>
+                  </ul>
+                </li>
+                <li className="subject__item">
+                  <button type="button" className="head">
+                    <span className="label">E-banking</span>
+
+                    <div className="controls">
+                      {/*<span className="count">3</span>*/}
+                      <ArrowSvg />
+                    </div>
+                  </button>
+
+                  <ul className="topic__list">
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Good performance" />
+                    </li>
+
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Low commissions" />
+                    </li>
+
+                    <li className="topic__item">
+                      <CheckboxInput withFill labelText="Low fees" />
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </aside>
     );
 
