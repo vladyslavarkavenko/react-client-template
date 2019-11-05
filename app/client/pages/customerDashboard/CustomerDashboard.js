15540ce7d8edfb0ff39e9ca89b19336844729333
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-05 14:44:21 +0200

    CustomerDashboard page initial

---
+++ app/client/pages/customerDashboard/CustomerDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ShiftedHeader from '../../components/ui-components/Layout/ShiftedHeader';
+
+export default function CustomerDashboard() {
+  return (
+    <section className="customer-dashboard">
+      <ShiftedHeader title="Dashboard" />
+      <div>SOME CONTENT</div>
+    </section>
+  );
+}
