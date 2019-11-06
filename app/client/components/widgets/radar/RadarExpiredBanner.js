b936e9a78428eae1e4594b345b30fdb5354516cf
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-06 17:44:40 +0200

    RadarExpiredBanner Logic

--- app/client/components/widgets/radar/RadarExpiredBanner.js
+++ app/client/components/widgets/radar/RadarExpiredBanner.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import ProgressBar from '../../ui-components/ProgressBar';
 import routing from '../../../utils/routing';
+import shareOpinionSelectors from '../../../modules/shareOpinion/shareOpinionSelectors';
 
-export default function RadarExpiredBanner({ selected }) {
-  if (!selected) {
+function RadarExpiredBanner({ selected, expiredCount }) {
+  if (!selected || !expiredCount) {
     return null;
   }
 
   const { label, id, type } = selected;
+
+  const total = 10;
+
+  const percentage = Math.round((expiredCount / total) * 100);
 
   return (
     <div className="radar-banner">
@@ -17,8 +23,28 @@ export default function RadarExpiredBanner({ selected }) {
         Update Now
       </Link>
       <p className="radar-banner__title">{label}</p>
-      <p className="radar-banner__subtitle">8 of 10 feedbacks has lost its impact</p>
-      <ProgressBar className="white" percentage={50} />
+      <p className="radar-banner__subtitle">
+        {expiredCount} of {total} feedbacks has lost its impact
+      </p>
+      <ProgressBar className="white" percentage={percentage} />
     </div>
   );
 }
+
+const mapStateToProps = (state, { selected }) => {
+  let expiredCount = 0;
+
+  if (selected && selected.id && selected.type) {
+    const expired = shareOpinionSelectors.getGlobalExpired(state);
+
+    try {
+      expiredCount = Object.keys(expired[selected.type.toUpperCase()][selected.id]).length;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return { expiredCount };
+};
+
+export default connect(mapStateToProps)(RadarExpiredBanner);
