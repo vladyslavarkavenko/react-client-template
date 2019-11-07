d727f91a4cf930ce91358da9994dc015d5453fbe
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-07 17:59:28 +0200

    Add ParticipationCircle loading overlay

--- app/client/components/widgets/ParticipationCircle.js
+++ app/client/components/widgets/ParticipationCircle.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CircleWithBackground from './CircleWithBackground';
+import { InlineSvgLoader } from '../ui-components/Layout/Loader';
 
 const config = {
   size: 300,
@@ -12,7 +13,7 @@ const config = {
   color: '#3EA0DA'
 };
 
-export default function ParticipationCircle({ data = {}, profileLabel = '' }) {
+export default function ParticipationCircle({ data = {}, profileLabel = '', status }) {
   const { numberOpinions, participationShare } = data;
 
   const { size } = config;
@@ -21,6 +22,12 @@ export default function ParticipationCircle({ data = {}, profileLabel = '' }) {
 
   return (
     <div className="participation">
+      {status === 'request' && (
+        <div className="screen-loader">
+          <InlineSvgLoader />
+        </div>
+      )}
+
       <div className="participation__chart">
         <svg viewBox={`0 0 ${size} ${size}`} width="100%" height="100%">
           <CircleWithBackground percent={percent} circle={config} config={config} />
