a1b160938795e97694ef36f551f876ab40fe5b68
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-12 19:36:29 +0200

    eslint fixes [2]

--- app/client/modules/opinionDetails/helpers/generateDomain.js
+++ app/client/modules/opinionDetails/helpers/generateDomain.js
@@ -1,20 +1,22 @@
-import { DATE_OFFSET } from './constants';
 import { addMonths, eachDayOfInterval } from 'date-fns';
+
+import { DATE_OFFSET } from './constants';
 
 export default function generateDomain(dateOffset, minDate, maxDate) {
   const start = new Date(minDate);
   const end = new Date(maxDate);
   let domain = [];
 
+  /* eslint-disable */
   switch (dateOffset) {
+    case DATE_OFFSET.MONTH:
+    case DATE_OFFSET.WEEK:
+      domain = eachDayOfInterval({ start, end }).map((date) => date.getTime());
+      break;
     case DATE_OFFSET.YEAR:
       start.setDate(1);
       const months = Array(12).fill(null);
       domain = months.map((_, index) => addMonths(start, index).getTime());
-      break;
-    case DATE_OFFSET.MONTH:
-    case DATE_OFFSET.WEEK:
-      domain = eachDayOfInterval({ start, end }).map((date) => date.getTime());
       break;
   }
 
