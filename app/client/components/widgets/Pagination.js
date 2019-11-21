215274ad3bc3150240f1d3789061999e1605790d
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-21 14:48:59 +0200

    UI Pagination component

---
+++ app/client/components/widgets/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+import ArrowUp from '../../../../public/assets/svg/arrow-up.regular.svg';
+
+/* eslint-disable */
+
+export default class Pagination extends React.Component {
+  render() {
+    // const { }
+
+    return (
+      <div className="pagination">
+        <ReactPaginate
+          pageCount={20}
+          containerClassName="pagination__list"
+          pageClassName="pagination__page"
+          breakClassName="pagination__break"
+          nextClassName="pagination__next"
+          previousClassName="pagination__prev"
+          breakLinkClassName="pagination__link"
+          pageLinkClassName="pagination__link"
+          previousLinkClassName="pagination__link"
+          nextLinkClassName="pagination__link"
+          activeClassName="active"
+          nextLabel={<ArrowUp />}
+          previousLabel={<ArrowUp />}
+        />
+      </div>
+    );
+  }
+}
